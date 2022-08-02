110393a83b9c9894d123012f28577a38f14aa28a
Ben Loy <bennbenbenl@gmail.com>
2022-08-03 00:56:12 +0800

    init commit

---
+++ controllers/pages/PageController.js
@@ -0,0 +1,6 @@
+const pageController = {
+    homePage: (req, res) => { res.render("pages/home") },
+    contactsPage: (req, res) => { res.render("pages/contacts") }
+};
+
+module.exports = pageController;
